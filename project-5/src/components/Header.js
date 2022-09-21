add990976be6ce290d0761b70c3b07b499a27364
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-21 22:36:50 +0300

    created the Header component

---
+++ project-5/src/components/Header.js
@@ -0,0 +1,11 @@
+import Joker from "../images/troller-face.png";
+
+export default function Navbar() {
+  return (
+    <nav className="header">
+      <img className="header-icon" src={Joker} alt="Troll Face" />
+      <h3 className="header-title">Meme Generator</h3>
+      <p className="header-text">React course - Project 3</p>
+    </nav>
+  );
+}
